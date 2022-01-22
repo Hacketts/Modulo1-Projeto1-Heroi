5591f6594f8c6f5951a4988637998fdb2c11855a
Nome de Usuário Hacketts <kalitahackett@gmail.com>
2022-01-21 23:03:38 -0300

    Envio de Projeto Jornada do Heroi

--- projeto.js
+++ projeto.js
@@ -1,26 +1,29 @@
 const prompt = require("prompt-sync")();
 
 console.log(
-`Um dia na Ilha Tenrou em X679 Red Lizard uma guilda de fadas foi atacada por outra Guilda chamada Blue Skull,
+  `Um dia na Ilha Tenrou no ano X679 Red Lizard uma guilda de fadas foi atacada por outra Guilda chamada Blue Skull,
 sendo derrotados cruelmente. Zera ficou presa sob escombros sendo salva pela limpadora do local Mavis que tinha 
 poderes ainda não descobertos e que sempre, 
 sempre acreditava um dia poder encontrar uma fada de (verdade).
-Assim, surgiu uma grande amizade,e um desejo de poder e vingança.`
+Assim, surgiu uma grande amizade,e um desejo imenso de desbravar o mundo das fadas.
+A jornada das duas foi marcada por novas pessoas e pela formação de uma guilda que
+fundaram com ajuda de seus novos amigos, a tão sonhada Fairy Tail a qual Mavis se tornou Mestre fundadora.
+E assim, até então a jornada de ambas havia chegado ao fim...ou não. `
 );
 
-var Perg1 = prompt(
+let Perg1 = prompt(
   "Mavis, em algum momento pensou em deixar Zera para trás?:  "
 );
-var Perg2 = prompt("Diga... você ainda acredita em fadas?:  ");
-var Perg3 = prompt(
+let Perg2 = prompt("Diga... você ainda acredita em fadas?:  ");
+
+let Perg3 = prompt(
   "Blue Skull era uma das guildas mais poderosas de Magnolia?:  "
 );
-var Perg4 = prompt(
-  "Mavis chegou a descobrir qual era seu verdadeiro poder?:  "
-);
-var Perg5 = prompt("Zera continuou a jornada até o final com Mavis?:  ");
+let Perg4 = prompt("Mavis chegou a descobrir qual era seu verdadeiro poder?: ");
 
-var Contador = 0;
+let Perg5 = prompt("Zera continuou a jornada até o final com Mavis?:  ");
+
+let Contador = 0;
 
 if (Perg1 == "sim") {
   Contador = Contador + 1;
